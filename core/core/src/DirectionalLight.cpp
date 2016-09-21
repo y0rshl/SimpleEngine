179673ee92c130c389bb370bc18f3a5834a113d2
Andrea Lata <andrelata12@gmail.com>
2016-09-21 17:44:50 -0300

    agrego el meted getDir a la directionalLight

--- core/core/src/DirectionalLight.cpp
+++ core/core/src/DirectionalLight.cpp
@@ -7,4 +7,14 @@
 DirectionalLight::DirectionalLight() {
 }
 
+/*Vec4 DirectionalLight::getDirLight () {
+    //Muliplico TRS * (0,0,1)
+    shared_ptr<SceneObject> light;
+
+    light = this->owner.lock();
+    SceneObject *so = light.get();
+    Matrix4x4 *trs = so->getPosition();
+
+    return trs->multVec4(this->vec4);
+}*/
 
