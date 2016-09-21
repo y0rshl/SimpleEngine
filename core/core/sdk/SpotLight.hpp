317b5a1a37ebc59c6cede7f05063a0f0fddeb07f
Andrea Lata <andrelata12@gmail.com>
2016-09-21 17:34:19 -0300

    agrego spotlight

---
+++ core/core/sdk/SpotLight.hpp
@@ -0,0 +1,19 @@
+//
+// Created by Andrea Lata on 9/21/16.
+//
+
+#ifndef SIMPLEENGINE_SPOTLIGHT_HPP
+#define SIMPLEENGINE_SPOTLIGHT_HPP
+
+
+#include "Light.hpp"
+
+class SpotLight : public Light {
+
+public:
+    SpotLight();
+
+};
+
+
+#endif //SIMPLEENGINE_SPOTLIGHT_HPP
---
+++ core/core/src/SpotLight.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Andrea Lata on 9/21/16.
+//
+
+#include "SpotLight.hpp"
+
+SpotLight::SpotLight () {
+
+}
